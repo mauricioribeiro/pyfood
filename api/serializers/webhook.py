c0e2b42bcb8203c7af6e4f08107161e1a173bd53
Mauricio Ribeiro <mauricio@justto.com>
2018-01-13 18:12:49 -0200

    webhook serializer obtaining data from request

--- api/serializers/webhook.py
+++ api/serializers/webhook.py
@@ -1,4 +1,42 @@
+import json
+import logging
+
+
 class WebhookSerializer:
 
     def __init__(self, request):
-        pass
+        data = None
+
+        try:
+            data = json.loads(request.body.decode("utf-8"))
+        except Exception as e:
+            logger = logging.getLogger(__name__)
+            logger.error("Fail to get webhook request data: " + e.__str__())
+
+        if data:
+            if 'originalRequest' in data:
+                original_request = data['originalRequest']
+
+                if 'source' in original_request:
+                    self.source = original_request['source']
+
+                if 'data' in original_request:
+                    if 'sender' in original_request['data'] and 'id' in original_request['data']['sender']:
+                        self.sender_id = original_request['data']['sender']['id']
+
+                    if 'message' in original_request['data'] and 'text' in original_request['data']['message']:
+                        self.message = original_request['data']['message']['text']
+
+            if 'result' in data:
+                if 'action' in data['result']:
+                    self.action = data['result']['action']
+
+            if 'parameters' in data and data['parameters']:
+                self.parameters = data['parameters']
+
+            if 'id' in data:
+                self.id = data['id']
+
+            if 'sessionId' in data:
+                self.session_id = data['sessionId']
+
--- api/views/webhook.py
+++ api/views/webhook.py
@@ -3,8 +3,11 @@ from rest_framework import generics
 
 from rest_framework.response import Response
 
+from api.serializers.webhook import WebhookSerializer
+
 
 class WebhookView(generics.CreateAPIView):
 
     def create(self, request, *args, **kwargs):
+        data = WebhookSerializer(request)
         return Response({}, OK)
